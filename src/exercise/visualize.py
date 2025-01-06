6aa1dd615b51458a2a69fb7e3437a132c9b08114
s204431 <s204431@student.dtu.dk>
2025-01-06 14:04:06 +0100

    Ruff

--- src/exercise/visualize.py
+++ src/exercise/visualize.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import torch
 import typer
-from exercise.model import MyAwesomeModel
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+
+from exercise.model import MyAwesomeModel
 
 
 def visualize(model_checkpoint: str, figure_name: str = "embeddings.png") -> None:
